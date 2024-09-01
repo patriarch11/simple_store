242e254d306438e70ea5c97f62f75793a9f6c9b7
Oleksii Zaichenko <o5zaichenko@gmail.com>
2024-09-01 11:00:19 +0300

    updated env

--- src/config.py
+++ src/config.py
@@ -11,9 +11,9 @@ class Config(BaseSettings):
 	DEBUG              : bool        = False
 	ENVIRONMENT        : Environment = Environment.PRODUCTION
 
-	CORS_ORIGINS       : list[str]
+	CORS_ORIGINS       : list[str]   = ['*']
 	CORS_ORIGINS_REGEX : str | None  = None
-	CORS_HEADERS       : list[str]
+	CORS_HEADERS       : list[str]   = ['*']
 
 	DATABASE_URL       : PostgresDsn
 
@@ -26,4 +26,3 @@ app_configs: dict[str, Any] = {
 
 if not settings.ENVIRONMENT.is_debug:
 	app_configs['openapi_url'] = None  # hide docs
-	
